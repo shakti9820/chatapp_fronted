3c45659211744e7edf0656a1797c74e343f9f9f5
shakti <shaktisingh979222@gmail.com>
2022-08-08 21:37:45 +0530

    changed function to async

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -11,14 +11,17 @@ export default function ChatContainer({ currentChat, socket }) {
   const scrollRef = useRef();
   const [arrivalMessage, setArrivalMessage] = useState(null);
 
-  useEffect(async () => {
-    const data = await JSON.parse(
-      localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
-    );
-    const response = await axios.post(recieveMessageRoute, {
-      from: data._id,
-      to: currentChat._id,
-    });
+  useEffect(() => {
+    const func=async()=>{
+      const data = await JSON.parse(
+        localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+      );
+      const response = await axios.post(recieveMessageRoute, {
+        from: data._id,
+        to: currentChat._id,
+      });
+    }
+    func();
     setMessages(response.data);
   }, [currentChat]);
 
